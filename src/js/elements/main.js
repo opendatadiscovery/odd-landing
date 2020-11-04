2ee0c1d53c7fc7d348b71223fd899d7d6e0b206b
Leshe4ka <kozyurov.alexey@gmail.com>
2020-11-04 15:58:20 +0300

    working

--- src/js/elements/main.js
+++ src/js/elements/main.js
@@ -1,6 +1,15 @@
 'use strict'
 
-function view(n) {
+/* let clicker = document.querySelectorAll('.click');
+let hidden = document.querySelectorAll('.hidden'); */
+
+let view = function(n) {
     style = document.getElementById(n).style;
     style.display = (style.display == 'block') ? 'none' : 'block';
 }
+
+onclick="view('hidden1'); return false"
+
+/* clicker.onclick = function() {
+    hidden.classList.toggle('shown')
+} */
