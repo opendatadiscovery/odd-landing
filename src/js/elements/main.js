da5f95df43ccff6ad99c134fd94f84318d43de37
Leshe4ka <kozyurov.alexey@gmail.com>
2020-11-09 22:16:00 +0300

    edited

--- src/js/elements/main.js
+++ src/js/elements/main.js
@@ -1,45 +1,7 @@
 'use strict'
 
-/* function foo() {
-    this.count++
-}
-  
-let Foo = {
-    count: 0,
-}
-  
-for(let i = 0; i < 10; i++) {
-    foo.bind(Foo)()
-    console.assert(Foo.count === 10)
-} */
-  
-  
 
-//////////////////////////////////////////////
 
-/* let foo = {
-    count: 0,
-    foo() {
-        this.count++;
-    }
-}
-  
-  for(let i = 0; i < 10; i++) {
-    foo.foo()
-    console.assert(foo.count === 10)
-} */
 
-//////////////////////////////////////////////
 
-/* let foo = {
-    foo() {
-        this.count++;
-    }
-}
 
-foo.count = 0;
-  
-  for(let i = 0; i < 10; i++) {
-    foo.foo()
-    console.assert(foo.count === 10)
-} */
