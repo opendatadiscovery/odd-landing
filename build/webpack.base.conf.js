35c564b3bbe25e18bb8ab25c62bc4cc94d03baba
Andrey Nenashev <anenashev90@gmail.com>
2023-10-17 19:19:28 +0200

    add Provectus section

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -80,6 +80,10 @@ module.exports = {
             }
           },
           {
+            loader: 'resolve-url-loader',
+            options: { sourceMap: true, }
+          },
+          {
             loader: 'sass-loader',
             options: { sourceMap: true }
           }
